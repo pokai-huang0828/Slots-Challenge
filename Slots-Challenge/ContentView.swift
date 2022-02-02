3f602086124ddf04e709c8ff6cf273e6ce3f288d
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-02-02 00:23:04 -0800

    Initial Commit

---
+++ Slots-Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots-Challenge
+//
+//  Created by PoKai Huang on 2022-02-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slots-Challenge/Slots_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_ChallengeApp.swift
+//  Slots-Challenge
+//
+//  Created by PoKai Huang on 2022-02-02.
+//
+
+import SwiftUI
+
+@main
+struct Slots_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
