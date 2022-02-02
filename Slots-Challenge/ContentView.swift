1718dc97d61e271f03656cd651ef1fa2619a53e2
Po-Kai Huang <71723684+pokai-huang0828@users.noreply.github.com>
2022-02-02 01:13:58 -0800

    Update Slots app

--- Slots-Challenge/ContentView.swift
+++ Slots-Challenge/ContentView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var credits = 1000
+    @State var slot1 = 1
+    @State var slot2 = 1
+    @State var slot3 = 1
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Image("background")
+                .resizable()
+                
+            VStack {
+                Text("SwiftUI Slots")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .padding(.all, 10)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                    
+                Text("Credits: \(credits)")
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color.white)
+                    .padding(.all, 10)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                if credits <= 0 {
+                    Text("Sorry... You Lose.")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.white)
+                        .padding(.all, 10)
+                        .background(Color.gray)
+                        .cornerRadius(10)
+                }
+                HStack{
+                    
+                    Image("fruit\(slot1)")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .scaledToFit()
+                        
+                    Image("fruit\(slot2)")
+                        .resizable()
+                        .scaledToFit()
+                    Image("fruit\(slot3)")
+                        .resizable()
+                        .scaledToFit()
+                    
+                }
+                Spacer()
+                Button {
+                    slot1 = Int.random(in: 1...3)
+                    slot2 = Int.random(in: 1...3)
+                    slot3 = Int.random(in: 1...3)
+                    
+                    if slot1 == slot2 && slot2 == slot3{
+                        credits += 25
+                    }
+                    else {
+                        credits -= 50
+                    }
+                    
+                } label: {
+                    Text("Spin")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding(.all, 20.0)
+                        .background(Color.gray)
+                        .cornerRadius(20)
+                }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all, 15.0/*@END_MENU_TOKEN@*/)
+        }
     }
 }
 
